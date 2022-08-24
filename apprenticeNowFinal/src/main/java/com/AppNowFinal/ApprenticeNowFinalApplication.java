60d096505aea8895af17d2eb36e85ac35af692c3
lvaladez1 <110848866+lvaladez1@users.noreply.github.com>
2022-08-24 07:17:50 -0700

    Add files via upload

---
+++ apprenticeNowFinal/src/main/java/com/AppNowFinal/ApprenticeNowFinalApplication.java
@@ -0,0 +1,13 @@
+package com.AppNowFinal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApprenticeNowFinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApprenticeNowFinalApplication.class, args);
+	}
+
+}
---
+++ apprenticeNowFinal/src/test/java/com/AppNowFinal/ApprenticeNowFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.AppNowFinal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApprenticeNowFinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
